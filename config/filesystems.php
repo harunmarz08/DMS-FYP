27efe7f596707d692be762d2a76f3dc8366a4af0
harunmarz08 <85564819+harunmarz08@users.noreply.github.com>
2024-06-03 16:36:38 +0800

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,16 +40,6 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
-            'permissions' => [
-                'file' => [
-                    'public' => 0664,
-                    'private' => 0600,
-                ],
-                'dir' => [
-                    'public' => 0775,
-                    'private' => 0700,
-                ],
-            ],
             'visibility' => 'public',
             'throw' => false,
         ],
